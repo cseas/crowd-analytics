d71137c2ab8aea828045194f552d3cbd0eb0f4be
abhijeetstomar <contact.abhijeetsingh@gmail.com>
2018-04-26 07:48:02 +0530

    Added comments

--- algo/ml.py
+++ algo/ml.py
@@ -5,6 +5,7 @@ from sklearn.linear_model import Ridge
 import cv2
 import multiprocessing.pool as mpool
 
+# function to analyse captured frame
 def func(image_data):
     response = requests.post(face_api_url, params=params, headers=headers, data=image_data)
     response.raise_for_status()
@@ -26,7 +27,7 @@ def func(image_data):
     print(diic)
     return diic
 
-
+# api code
 subscription_key = "02726400482345229652709041c698ba"
 assert subscription_key
 
@@ -39,45 +40,50 @@ params = {
     'returnFaceAttributes': 'emotion',
 }
 
-
+# scikit-learn code
 X = np.loadtxt('Xval.txt', dtype=float)
 y = np.loadtxt('yval.txt', dtype=int)
 
 RigeModel = Ridge(alpha = 0.001)
 RigeModel.fit(X, y)
+
+# opencv code
 cap = cv2.VideoCapture(0)
 # set the width and height, and UNSUCCESSFULLY set the exposure time
 cap.set(3, 1280)
 cap.set(4, 1024)
 cap.set(15, 0.1)
+
+# multi-threading using pooled threads
 pool = mpool.ThreadPool(100)
+
 while True:
-        ret, img = cap.read()
-        img = cv2.flip(img, 1)
-        cv2.imshow("input", img)
-        # cv2.imshow("thresholded", imgray*thresh2)
-        cv2.imwrite("test.bmp", img)  # writes image test.bmp to disk
-        image_data = open("test.bmp", "rb").read()
-        result = pool.apply_async(func, args=(image_data,))
-        try:
-            diic = result.get(timeout=None)
-            if (diic != []):
-                Yhat = RigeModel.predict(np.array(diic))
-                print("Yhat", Yhat)
-                rSquare = RigeModel.score(X, y)
-                print("R^2", rSquare)
+    ret, img = cap.read()
+    img = cv2.flip(img, 1)
+    cv2.imshow("input", img) # displays video being captured
+    # cv2.imshow("thresholded", imgray*thresh2)
+    cv2.imwrite("test.bmp", img)  # writes image test.bmp to disk
+    image_data = open("test.bmp", "rb").read()
+    result = pool.apply_async(func, args=(image_data,))
 
-        except:
-            print("execption occured")
+    try:
+        diic = result.get(timeout=None)
+        if (diic != []):
+            Yhat = RigeModel.predict(np.array(diic))
+            print("Yhat", Yhat)
+            rSquare = RigeModel.score(X, y)
+            print("R^2", rSquare)
 
+    except:
+        print("execption occured")
 
-        os.remove("test.bmp")
-        key = cv2.waitKey(9)
-        if key == 27:  # Esc key
-            break
+    os.remove("test.bmp")
+    key = cv2.waitKey(9)
+    if key == 27:  # Esc key
+        break
 
-cv2.destroyAllWindows()
-cv2.VideoCapture(0).release()
+cv2.destroyAllWindows() # close video stream window
+cv2.VideoCapture(0).release() # stop capturing video
+
 pool.close()
 pool.join()
-
