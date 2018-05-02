27d62a2a3a30b43061b66d8e36ae3926af698b7f
abhijeetstomar <contact.abhijeetsingh@gmail.com>
2018-05-02 22:38:54 +0530

    Added comments to graph code

--- algo/graph.py
+++ algo/graph.py
@@ -1,14 +1,17 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+# load y-axis values from graphy.txt
 y = np.loadtxt('graphy.txt', dtype=int)
-
-# load x-axis values from graphx.txt and convert epoch values to seconds
+# load x-axis values from graphx.txt
 x = np.loadtxt('graphx.txt', dtype=int)
+
+# convert epoch values to seconds
 temp = x[0]
 for i in range(len(x)):
     print(i)
     x[i] -= temp
-print(x)
+
+# plot graph
 plt.plot(x, y, marker='x')
 plt.show()
