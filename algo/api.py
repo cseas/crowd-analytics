e799cb15733e37887d4e71d497e7310af52e362c
abhijeetstomar <contact.abhijeetsingh@gmail.com>
2018-05-12 00:29:33 +0530

    Removed old dataset and fixed trailing whitespace issue

--- algo/api.py
+++ algo/api.py
@@ -1,7 +1,6 @@
 import os
 import requests
 import numpy as np
-# from sklearn.linear_model import Ridge
 
 # free, 20 calls/min, limit 30K calls/month
 # subscription_key = "bf5951c5f4934e2e90bc11c48ffb57fa"
@@ -21,17 +20,16 @@ params = {
 X = np.empty((0, 8), float)
 y = np.empty((0, 1), float)
 
-# traverse file
-# indir = '/home/abhijeet/Documents/faceapi/algo'
+# dataset folder
 indir = '/home/abhijeet/Documents/github/crowd-analytics/algo'
-#for reoot, dirs, filenames in os.walk(indir):
+
 for dirs,dirlist,filenames in os.walk("."):
     print(dirs)
 
     for filename in filenames:
+        print(filename)
         if filename.endswith(".jpeg") or filename.endswith(".jpg") or filename.endswith(".png"):
             # print(os.path.join(directory, filename))
-            #print(filename)
             # image_data = open(indir + '/' + filename, "rb").read()
             image_data = open(indir + '/' + dirs.split('/')[1]+'/'+filename, "rb").read()
 
@@ -42,10 +40,6 @@ for dirs,dirlist,filenames in os.walk("."):
             for i in analysis:
 
                 dic = []
-                # j = key
-                # value = value of emotion
-                # for j, value in i["faceAttributes"]["emotion"].items():
-                #     dic.insert(len(dic), value)
 
                 dic.insert(len(dic), i["faceAttributes"]["emotion"]["anger"])
                 dic.insert(len(dic), i["faceAttributes"]["emotion"]["contempt"])
@@ -56,15 +50,17 @@ for dirs,dirlist,filenames in os.walk("."):
                 dic.insert(len(dic), i["faceAttributes"]["emotion"]["sadness"])
                 dic.insert(len(dic), i["faceAttributes"]["emotion"]["surprise"])
 
-                arr=np.array(dic)
-                # print(dic)
-                # print(i["faceAttributes"]["emotion"])
+                # convert list to numpy array
+                arr = np.array(dic)
+
+                # insert next row to main numpy array
                 X = np.vstack([X, arr])
+
                 # print(dirs)
                 # print("---",dirs[2:],"----")
                 # print(type(dirs))
 
-                if str(dirs[2:]) == "bored ":
+                if str(dirs[2:]) == "bored":
                     y = np.insert(y, len(y), 1)
                 else:
                     y = np.insert(y, len(y), -1)
@@ -74,10 +70,3 @@ for dirs,dirlist,filenames in os.walk("."):
 
 np.savetxt('Xval.txt', X, fmt='%f')
 np.savetxt('yval.txt', y, fmt='%d')
-# scikit code
-# print(X)
-# print(y)
-# RigeModel = Ridge(alpha = 0.1)
-# RigeModel.fit(X, y)
-# Yhat = RigeModel.predict(np.array([[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0]]))
-# print("Yhat", Yhat)
